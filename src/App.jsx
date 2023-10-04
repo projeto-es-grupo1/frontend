d6aa3a659dbdc8b716a85a93778eb1e1256437a8
AlexWasHeree <victoralexandre2303@gmail.com>
2023-10-04 17:36:44 -0300

    configurando rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import Home from './pages/Home';
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Home />} path="/" exact />
+          <Route element={<Login />} path="/login" />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
