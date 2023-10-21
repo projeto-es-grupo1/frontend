4e8a2cc235e9fa77c846f8d2af35d850f016d906
AlexWasHeree <victoralexandre2303@gmail.com>
2023-10-21 17:36:01 -0300

    fix: estilos de login page e register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Button from '../components/Button';
+import { CssBaseline } from '@mui/material';
 
 const Register = () => {
   const [email, setEmail] = useState('');
@@ -33,87 +34,95 @@ const Register = () => {
   };
 
   return (
-    <div style={styles.background}>
-      <div style={styles.loginContainer}>
-        <div style={styles.titleContainer}>
-          <h2 style={styles.title}>Registrar</h2>
-        </div>
-        
-        <div style={styles.inputContainer}>
-          <form onSubmit={handleSubmit}>
-          <div className="form-group">
-              <input
-                type="email"
-                id="email"
-                value={name}
-                onChange={handleNameChange}
-                style={styles.input}
-                placeholder='Name'
-              />
-            </div>
-            <div className="form-group">
-              <input
-                type="email"
-                id="email"
-                value={email}
-                onChange={handleEmailChange}
-                style={styles.input}
-                placeholder='Email'
-              />
-            </div>
-            <div className="form-group">
-              <input
-                type="password"
-                id="senha"
-                value={senha}
-                onChange={handleSenhaChange}
-                style={styles.input}
-                placeholder='Senha'
-              />
-            </div>            
-            <div className="form-group">
-              <input
-                type="password"
-                id="confirmarSenha"
-                value={senha}
-                onChange={handleSenhaChange}
-                style={styles.input}
-                placeholder='Confirmar Senha'
-              />
-            </div>
-            
-            <div className="form-group" style={{ display: 'flex', justifyContent: 'space-between' }}>
-              <label htmlFor="contaPessoal">Conta pessoal</label>
-              <input
-                type="checkbox"
-                id="contaPessoal"
-                checked={contaPessoal}
-                onChange={handleContaPessoalChange}
-              />
-              
-              <label htmlFor="contaInstitucional">Conta institucional</label>
-              <input
-                type="checkbox"
-                id="contaInstitucional"
-                checked={contaInstitucional}
-                onChange={handleContaInstitucionalChange}
-              />
-            </div>
-          </form>
-        </div>
-        
-        <div style={styles.footer}>
-          <Button texto="Cadastrar" onClick={handleSubmit}></Button>
-          <a style={styles.link} href="#">Voltar para login</a>
+    <>
+      <CssBaseline />
+      <div style={styles.background}>
+        <div style={styles.loginContainer}>
+          <div style={styles.titleContainer}>
+            <h2 style={styles.title}>Registrar</h2>
+          </div>
+
+          <div style={styles.inputContainer}>
+            <form onSubmit={handleSubmit}>
+              <div className="form-group">
+                <input
+                  type="email"
+                  id="email"
+                  value={name}
+                  onChange={handleNameChange}
+                  style={styles.input}
+                  placeholder="Name"
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="email"
+                  id="email"
+                  value={email}
+                  onChange={handleEmailChange}
+                  style={styles.input}
+                  placeholder="Email"
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="password"
+                  id="senha"
+                  value={senha}
+                  onChange={handleSenhaChange}
+                  style={styles.input}
+                  placeholder="Senha"
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="password"
+                  id="confirmarSenha"
+                  value={senha}
+                  onChange={handleSenhaChange}
+                  style={styles.input}
+                  placeholder="Confirmar Senha"
+                />
+              </div>
+
+              <div
+                className="form-group"
+                style={{ display: 'flex', justifyContent: 'space-between' }}
+              >
+                <label htmlFor="contaPessoal">Conta pessoal</label>
+                <input
+                  type="checkbox"
+                  id="contaPessoal"
+                  checked={contaPessoal}
+                  onChange={handleContaPessoalChange}
+                />
+
+                <label htmlFor="contaInstitucional">Conta institucional</label>
+                <input
+                  type="checkbox"
+                  id="contaInstitucional"
+                  checked={contaInstitucional}
+                  onChange={handleContaInstitucionalChange}
+                />
+              </div>
+            </form>
+          </div>
+
+          <div style={styles.footer}>
+            <Button texto="Cadastrar" onClick={handleSubmit}></Button>
+            <a style={styles.link} href="#">
+              Voltar para login
+            </a>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
 const styles = {
   background: {
-    backgroundColor: '#e0e0e0',
+    backgroundColor: '#E2E5E9',
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
@@ -130,10 +139,10 @@ const styles = {
     boxShadow: '0 4px 10px rgba(0, 0, 0, 0.1)',
     textAlign: 'center',
     width: 419,
-    height: 562
+    height: 562,
   },
   title: {
-    color: '#3F3F3F'
+    color: '#3F3F3F',
   },
   inputContainer: {
     marginTop: -100,
@@ -157,14 +166,14 @@ const styles = {
     alignItems: 'center',
     justifyContent: 'space-between',
     marginBottom: 3,
-    marginTop: -150
+    marginTop: -150,
   },
   link: {
-    marginTop: 20, 
+    marginTop: 20,
     color: '#5972F5',
     fontWeight: 400,
-    fontSize: 14
-  }
+    fontSize: 14,
+  },
 };
 
 export default Register;
